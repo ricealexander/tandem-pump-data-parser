f14f6a7c2755e99fe91c83ae6d0fa96245639446
ricealexander <webdev@ricealexander>
2020-01-07 22:28:14 -0600

    :construction: read Data and split it into sections for each table
    :triangular_ruler: Data is delimeted by empty lines. First section is metadata. Subsequent sections are individual CSV tables

--- main.js
+++ main.js
@@ -2,9 +2,28 @@ import axios from 'axios'
 
 const CSV_FILES_PATH = '/data'
 
-async function parseData (url) {
-  const request = await axios.get(`${CSV_FILES_PATH}/${url}`)
-  console.log(request)
+async function parseTandemData (url) {
+  const { data, headers } = await axios.get(`${CSV_FILES_PATH}/${url}`)
+  const isCSV = headers['content-type'].includes('text/csv')
+
+  if (!isCSV) {
+    throw new Error('Could not resolve provided data file as CSV')
+  }
+
+  // Tandem tables are delimeted with empty lines
+  const sections = data
+    .replace(/\r\n|\r/, '\n') // normalize line-endings
+    .split(/\n(\s+)?\n/)      // split on empty lines
+    .filter(section => {      // remove empty lines from array
+      // empty lines may include invisible characters
+      // section has content if it contains word characters or commas
+      const hasContent = string => /[\w,]/.test(string)
+      return hasContent(section)
+    })
+
+  const [metaData, ...CSVTables] = sections
+  return {metaData, CSVTables}
 }
 
-parseData('Tandem Data 20191113-20200106.csv')
+parseTandemData('Tandem Data 20191113-20200106.csv')
+  .then(data => console.log(data))
